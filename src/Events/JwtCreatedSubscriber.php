8e6b97198c5002ca7857afc99344d315579770a7
molodoi <mr_stat12@hotmail.com>
2021-10-12 23:53:07 +0200

    Add Events UserPasswordEncoder + Link Customer to User + InvoiceChrono && Repository + JwtCreated Add fieds to jwt token + Doctrine extension CurrentUser

---
+++ src/Events/JwtCreatedSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Events;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JwtCreatedSubscriber
+{
+    public function updateJwtData(JWTCreatedEvent $event)
+    {
+        // 1. Récupérer l'utilisateur (pour avoir son firstName et lastName)
+        /** @var User */
+        $user = $event->getUser();
+
+        // 2. Enrichir les data pour qu'elles contiennent ces données
+        $data = $event->getData();
+        $data['firstName'] = $user->getFirstName();
+        $data['lastName'] = $user->getLastName();
+
+        $event->setData($data);
+    }
+}
