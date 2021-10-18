5dadc63c0725d1709ba70509056fa2679375508c
molodoi <mr_stat12@hotmail.com>
2021-10-18 23:58:43 +0200

    Add Auth Form + Login/Logout + AuthState + Context + Routing

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,26 +2,46 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, Route, Switch, withRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+import AuthContext from "./contexts/AuthContext";
 import HomePage from "./pages/HomePage";
 import CustomersPage from "./pages/CustomersPage";
 import InvoicesPage from "./pages/InvoicesPage";
+import LoginPage from "./pages/LoginPage";
+import AuthAPI from "./services/authAPI";
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 
+AuthAPI.setup(); // check le token
+
 const App = () => {
+    
+    const [isAuthenticated, setIsAuthenticated] = useState(
+        AuthAPI.isAuthenticated()
+    );
+
+    const NavbarWithRouter = withRouter(Navbar);
 
     return (
-        <HashRouter>
-            <Navbar />
-            <main className="container pt-5">
-                <Switch>
-                    <Route path="/invoices" component={InvoicesPage} />
-                    <Route path="/customers" component={CustomersPage} />
-                    <Route path="/" component={HomePage} />
-                </Switch>
-            </main>
-        </HashRouter>
+        <AuthContext.Provider
+            value={{
+                isAuthenticated,
+                setIsAuthenticated
+            }}
+        >
+            <HashRouter>
+                <NavbarWithRouter />
+                <main className="container pt-5">
+                    <Switch>
+                        <Route path="/login" component={LoginPage} />
+                        <PrivateRoute path="/invoices" component={InvoicesPage} />
+                        <PrivateRoute path="/customers" component={CustomersPage} />
+                        <Route path="/" component={HomePage} />
+                    </Switch>
+                </main>
+            </HashRouter>
+        </AuthContext.Provider>
     );
 };
 
--- assets/js/components/Navbar.jsx
+++ assets/js/components/Navbar.jsx
@@ -1,43 +1,65 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import AuthAPI from "../services/authAPI";
+import AuthContext from "../contexts/AuthContext";
 
 const Navbar = ({ history }) => {
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <NavLink className="navbar-brand" to="/">
-                SymReact !
-            </NavLink>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+    const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
 
-            <div className="collapse navbar-collapse" id="navbarColor03">
-                <ul className="navbar-nav me-auto">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/customers">
-                            Clients
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/invoices">
-                            Factures
-                        </NavLink>
-                    </li>
-                </ul>
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item mx-1">
-                        <a className="btn btn-outline-primary btn-sm" href="#">Inscription</a>
-                    </li> 
-                    <li className="nav-item mx-1">
-                        <a className="btn btn-outline-secondary btn-sm" href="#">Connexion</a>
-                    </li> 
-                </ul>
+    const handleLogout = () => {
+        AuthAPI.logout();
+        setIsAuthenticated(false);
+        history.push("/login");
+    };
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <NavLink className="navbar-brand" to="/">
+                    SymReact !
+                </NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarColor03">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/customers">
+                                Clients
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/invoices">
+                                Factures
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav ml-auto">
+                        {(!isAuthenticated && (
+                            <>
+                                <li className="nav-item mx-1">
+                                    <a className="btn btn-outline-primary btn-sm" href="#">Inscription</a>
+                                </li> 
+                                <li className="nav-item mx-1">
+                                    <NavLink to="/login" className="btn btn-outline-secondary btn-sm">
+                                        Connexion !
+                                    </NavLink>
+                                </li> 
+                            </>
+                        )) || (
+                            <li className="nav-item mx-1">
+                                <button onClick={handleLogout} className="btn btn-danger btn-sm">
+                                    Déconnexion
+                                </button>
+                            </li>
+                        )}    
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-  );
+        </nav>
+    );
 };
 
 export default Navbar;
